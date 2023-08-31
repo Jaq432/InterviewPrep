9114b5bb3a5b429ffb118affb181bb9de30e77e8
Jacob Thornton <jacobreidth@hotmail.com>
2023-08-30 17:45:41 -0700

    Making an organizer

---
+++ FileOrganizer/Organizer.py
@@ -0,0 +1,46 @@
+# The goal of this program is to make a file organizer. 
+# In the folder is a myriad of different files that all need organizing.
+# It is our duty, if we choose to accept it, to put them where they belong.
+# (Where they belong is in folders corresponding to their extension)
+#
+# Hits at the bottom of the file
+
+
+
+
+fileExtensions = {
+    'pdf': "PDFs",
+    'png': "Images",
+    'jpg': "Images",
+    'jpeg': "Images",
+    'gif': 'Images',
+    'doc': 'Documents',
+    'docx': 'Documents',
+    'txt': 'Documents',
+    'csv': 'Data',
+    'xlsx': 'Data',
+    'zip': 'Archive',
+    'rar': 'Archive',
+    'exe': 'Executables',
+    'mp3': 'Music',
+    'wav': 'Music',
+    'mp4': 'Videos',
+    'avi': 'Videos',
+    'flv': 'Videos',
+    'wmv': 'Videos',
+}
+
+
+
+
+
+
+
+
+# Hints
+
+# Use the import os module to perform operating system functions
+# ----------------------------------------------------------
+# Use the os.getcwd() command to get the current file's location
+# ----------------------------------------------------------
+# 
